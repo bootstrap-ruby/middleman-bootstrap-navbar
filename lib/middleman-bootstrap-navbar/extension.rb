a94212ea102c8edbb0822b6c715fff771b5e438e
Manuel Meurer <manuel@krautcomputing.com>
2016-08-21 21:32:14 +0200

    refactor

--- lib/middleman-bootstrap-navbar/extension.rb
+++ lib/middleman-bootstrap-navbar/extension.rb
@@ -4,14 +4,12 @@ require 'middleman-core/version'
 module Middleman
   module BootstrapNavbar
     if Middleman::VERSION.split('.').first.to_i == 3
-      module Extension
-        class << self
-          def registered(app, options = {}, &block)
-            ::BootstrapNavbar.configure(&block) if block_given?
-            app.helpers ::BootstrapNavbar::Helpers
-          end
-          alias :included :registered
+      class Extension < Middleman::Extension
+        def registered(app)
+          ::BootstrapNavbar.configure(&block) if block_given?
+          app.helpers ::BootstrapNavbar::Helpers
         end
+        alias :included :registered
       end
     else
       class Extension < Extension
